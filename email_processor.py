85b526b89915df315f226db88bbf43a9d2814f54
Joni Honkanen <joge.honkanen@gmail.com>
2025-10-22 09:07:16 +0300

    Still updates for email processor

--- email_processor.py
+++ email_processor.py
@@ -8,7 +8,7 @@ from imapclient import IMAPClient
 from email.message import Message
 from dotenv import load_dotenv
 from typing import Any, Dict, List
-from mailparser_reply import EmailReplyParser
+from email_reply_parser import EmailReplyParser
 
 import psycopg
 from integrations.article_enrichment_integration import enrich_article_with_email_reply
